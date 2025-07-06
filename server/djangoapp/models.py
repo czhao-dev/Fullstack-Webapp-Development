db4e8192cb68b475bd8aa81ce289dbb75fe981eb
czhao-dev <charleszhao94@gmail.com>
2025-07-06 16:37:34 -0700

    Add source code for the project

---
+++ server/djangoapp/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('PICKUP', 'Pickup'),
+        ('HATCHBACK', 'Hatchback'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('MINIVAN', 'Minivan'),
+        ('CROSSOVER', 'Crossover'),
+        ('HYBRID', 'Hybrid'),
+        ('EV', 'EV'),
+        ('SUBCOMPACT', 'Subcompact'),
+        ('SPORTS', 'Sports'),
+    ]
+    type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2025,
+        validators=[
+            MaxValueValidator(2025),
+            MinValueValidator(2015)
+        ]
+    )
+
+    def __str__(self):
+        return self.name
